3b079b74711c8b6aa54e9e895573e7abe9090c0d
corzej <corzej@gmail.com>
2013-10-23 10:38:56 -0400

    Finish Static Pages

--- Gemfile
+++ Gemfile
@@ -7,11 +7,19 @@ gem 'rails', '4.0.0'
 group :development, :test do
   gem 'sqlite3', '1.3.8'
   gem 'rspec-rails', '2.13.1'
+  gem 'pry'
+  gem 'guard-rspec', '0.5.5'
+  gem 'spork-rails', '4.0.0'
+
 end
 
 group :test do
   gem 'selenium-webdriver', '2.35.1'
   gem 'capybara', '2.1.0'
+  gem 'rb-fsevent'
+  gem 'growl', '1.0.3'
+  gem 'guard-spork', '0.3.2'
+
 end
 
 gem 'sass-rails', '4.0.1'
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe "Static pages/" do
+	describe "Home page" do
+		it "should have the content 'Home'" do
+			visit '/static_pages/home'
+			expect(page).to have_selector('h1', :text => 'Home')
+			expect(page).to have_content('기도제목을 나누고 말씀을 나누는 창입니다.')
+		end
+		it "should have the title" do
+			visit '/static_pages/home'
+			expect(page).to have_title("기도제목 Web Sharing | Home")
+		end
+	end
+	describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_selector('h1', :text => 'Help')
+    end
+    it "should have the title" do
+			visit '/static_pages/help'
+			expect(page).to have_title("기도제목 Web Sharing | Help")
+		end
+  end
+	describe "About page" do
+
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_selector('h1', :text => 'About')
+    end
+    it "should have the title" do
+			visit '/static_pages/about'
+			expect(page).to have_title("기도제목 Web Sharing | About")
+		end
+  end
+end
