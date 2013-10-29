869bb984c62bb1761a7ae162e450327e23d06d1a
corzej <corzej@gmail.com>
2013-10-29 19:35:52 -0400

    Test file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,24 @@
 require 'spec_helper'
 
 describe "Static pages/" do
+
 	describe "Home page" do
+
 		it "should have the content 'Home'" do
 			visit '/static_pages/home'
 			expect(page).to have_selector('h1', :text => 'Home')
 			expect(page).to have_content('기도제목을 나누고 말씀을 나누는 창입니다.')
 		end
-		it "should have the title" do
+		it "should have the base title" do
 			visit '/static_pages/home'
-			expect(page).to have_title("기도제목 Web Sharing | Home")
+			expect(page).to have_title("기도제목 Web Sharing")
 		end
 	end
+		it "should not have a custom page title" do
+			visit '/static_pages/home'
+			expect(page).to_not have_title("| Home")
+		end
+
 	describe "Help page" do
 
     it "should have the content 'Help'" do
